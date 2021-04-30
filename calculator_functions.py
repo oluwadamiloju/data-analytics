cf57fa48a03b13135532d5294ab7e8848e731671
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-04-30 16:38:54 +0100

    calculator function

---
+++ calculator_functions.py
@@ -0,0 +1,14 @@
+def add(num1: int, num2: int) -> int:
+    return num1 + num2
+
+
+def sub(first_number: int, second_number: int) -> int:
+    return first_number - second_number
+
+
+def div(number1: int, number2: int) -> int:
+    return number1 // number2
+
+
+def mul(num1, num2):
+    return num1 * num2
